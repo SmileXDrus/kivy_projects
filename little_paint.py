cd276e3537d0a93386622751f33c31a1bedd8fb1
Мухин Владислав Николаевич <VMuhin@it-platforma.ru>
2021-02-09 10:14:22 +0300

    first_app

---
+++ little_paint.py
@@ -0,0 +1,45 @@
+#  When creating an application, you have to ask yourself three important questions:
+#         What data does my application process?
+#         How do I visually represent that data?
+#         How does the user interact with that data?
+# doc https://kivy.org/doc/stable/tutorials/firstwidget.html
+
+from random import random
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.widget import Widget
+from kivy.graphics import Color, Ellipse, Line
+
+
+class MyPaintWidget(Widget):
+    def on_touch_down(self, touch):
+        color = (random(), random(), random())
+        with self.canvas:
+            # Color(2, 1, 0)
+            # Color(2, 1, 0, .5)
+            Color(*color)
+            d = 30.
+            Ellipse(pos=(touch.x - d / 2, touch.y - d / 2), size=(d, d))
+            touch.ud['line'] = Line(points=(touch.x, touch.y))
+
+    def on_touch_move(self, touch):
+        touch.ud['line'].points += [touch.x, touch.y]
+
+
+class MyPaintApp(App):
+    def build(self):
+        parent = Widget()
+        self.painter = MyPaintWidget()
+        clearbtn = Button(text='Clear')
+        clearbtn.bind(on_release=self.clear_canvas)
+        parent.add_widget(self.painter)
+        parent.add_widget(clearbtn)
+        return parent
+
+    def clear_canvas(self, obj):
+        self.painter.canvas.clear()
+
+
+if __name__ == '__main__':
+    MyPaintApp().run()
